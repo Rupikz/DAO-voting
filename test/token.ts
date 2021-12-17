04ed799c054a4738fde0f18051197b3f2b42d4eb
rupikz <kydry.nikolau@gmail.com>
2021-12-17 06:57:20 +0300

    lint token test

--- test/token.ts
+++ test/token.ts
@@ -1,22 +1,19 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Token", function () {
   const decimals = 18;
+  let sender: SignerWithAddress,
+    recipient: SignerWithAddress,
+    account: SignerWithAddress,
+    accountOther: SignerWithAddress;
 
   beforeEach(async function () {
     const Token = await ethers.getContractFactory("Token");
     const token = await Token.deploy("Nikolai", "NIC", decimals);
     await token.deployed();
-    const [sender, recipient, account, accountOther] =
-      await ethers.getSigners();
-    const zeroSender = await ethers.getSigner(ethers.constants.AddressZero);
-
-    this.sender = sender;
-    this.recipient = recipient;
-    this.account = account.address;
-    this.accountOther = accountOther.address;
-    this.zeroSender = zeroSender;
+    [sender, recipient, account, accountOther] = await ethers.getSigners();
     this.token = token;
   });
 
@@ -28,22 +25,22 @@ describe("Token", function () {
   });
 
   it("Создание и удаление токенов", async function () {
-    expect(await this.token.balanceOf(this.account)).to.equal(0);
-    expect(await this.token.balanceOf(this.accountOther)).to.equal(0);
+    expect(await this.token.balanceOf(account.address)).to.equal(0);
+    expect(await this.token.balanceOf(accountOther.address)).to.equal(0);
     expect(await this.token.totalSupply()).to.equal(0);
 
-    await this.token.mint(this.account, 20);
-    await this.token.mint(this.accountOther, 10);
+    await this.token.mint(account.address, 20);
+    await this.token.mint(accountOther.address, 10);
 
-    expect(await this.token.balanceOf(this.account)).to.equal(20);
-    expect(await this.token.balanceOf(this.accountOther)).to.equal(10);
+    expect(await this.token.balanceOf(account.address)).to.equal(20);
+    expect(await this.token.balanceOf(accountOther.address)).to.equal(10);
     expect(await this.token.totalSupply()).to.equal(30);
 
-    await this.token.burn(this.account, 5);
-    await this.token.burn(this.accountOther, 5);
+    await this.token.burn(account.address, 5);
+    await this.token.burn(accountOther.address, 5);
 
-    expect(await this.token.balanceOf(this.account)).to.equal(15);
-    expect(await this.token.balanceOf(this.accountOther)).to.equal(5);
+    expect(await this.token.balanceOf(account.address)).to.equal(15);
+    expect(await this.token.balanceOf(accountOther.address)).to.equal(5);
     expect(await this.token.totalSupply()).to.equal(20);
 
     await expect(
@@ -52,90 +49,74 @@ describe("Token", function () {
     await expect(
       this.token.burn(ethers.constants.AddressZero, 5)
     ).to.be.revertedWith("ERC20: burn from the zero address");
-    await expect(this.token.burn(this.account, 20)).to.be.revertedWith(
+    await expect(this.token.burn(account.address, 20)).to.be.revertedWith(
       "ERC20: burn amount exceeds balance"
     );
   });
 
   it("Апрув токенов", async function () {
-    await this.token.connect(this.sender).approve(this.recipient.address, 20);
+    await this.token.approve(recipient.address, 20);
     expect(
-      await this.token.allowance(this.sender.address, this.recipient.address)
+      await this.token.allowance(sender.address, recipient.address)
     ).to.equal(20);
 
-    await this.token.connect(this.sender).approve(this.recipient.address, 5);
+    await this.token.approve(recipient.address, 5);
     expect(
-      await this.token.allowance(this.sender.address, this.recipient.address)
+      await this.token.allowance(sender.address, recipient.address)
     ).to.equal(5);
 
-    await this.token
-      .connect(this.sender)
-      .increaseAllowance(this.recipient.address, 5);
+    await this.token.increaseAllowance(recipient.address, 5);
     expect(
-      await this.token.allowance(this.sender.address, this.recipient.address)
+      await this.token.allowance(sender.address, recipient.address)
     ).to.equal(10);
 
-    await this.token
-      .connect(this.sender)
-      .decreaseAllowance(this.recipient.address, 3);
+    await this.token.decreaseAllowance(recipient.address, 3);
     expect(
-      await this.token.allowance(this.sender.address, this.recipient.address)
+      await this.token.allowance(sender.address, recipient.address)
     ).to.equal(7);
 
     await expect(
-      this.token
-        .connect(this.sender)
-        .decreaseAllowance(this.recipient.address, 10)
+      this.token.decreaseAllowance(recipient.address, 10)
     ).to.be.revertedWith("ERC20: decreased allowance below zero");
     await expect(
-      this.token
-        .connect(this.sender)
-        .increaseAllowance(ethers.constants.AddressZero, 10)
+      this.token.increaseAllowance(ethers.constants.AddressZero, 10)
     ).to.be.revertedWith("ERC20: approve to the zero address");
   });
 
   it("Перевод токенов", async function () {
-    await this.token.mint(this.sender.address, 20);
-    await this.token.approve(this.sender.address, 20);
-    await this.token.connect(this.sender).transfer(this.recipient.address, 6);
+    await this.token.mint(sender.address, 20);
+    await this.token.approve(sender.address, 20);
+    await this.token.transfer(recipient.address, 6);
 
-    expect(await this.token.balanceOf(this.sender.address)).to.equal(14);
-    expect(await this.token.balanceOf(this.recipient.address)).to.equal(6);
+    expect(await this.token.balanceOf(sender.address)).to.equal(14);
+    expect(await this.token.balanceOf(recipient.address)).to.equal(6);
 
-    await this.token.connect(this.recipient).transfer(this.sender.address, 1);
+    await this.token.connect(recipient).transfer(sender.address, 1);
 
-    expect(await this.token.balanceOf(this.sender.address)).to.equal(15);
-    expect(await this.token.balanceOf(this.recipient.address)).to.equal(5);
+    expect(await this.token.balanceOf(sender.address)).to.equal(15);
+    expect(await this.token.balanceOf(recipient.address)).to.equal(5);
 
-    await this.token.transferFrom(
-      this.sender.address,
-      this.recipient.address,
-      3
-    );
-    expect(await this.token.balanceOf(this.sender.address)).to.equal(12);
-    expect(await this.token.balanceOf(this.recipient.address)).to.equal(8);
+    await this.token.transferFrom(sender.address, recipient.address, 3);
+    expect(await this.token.balanceOf(sender.address)).to.equal(12);
+    expect(await this.token.balanceOf(recipient.address)).to.equal(8);
 
     await expect(
-      this.token
-        .connect(this.sender)
-        .transferFrom(ethers.constants.AddressZero, this.recipient.address, 10)
+      this.token.transferFrom(
+        ethers.constants.AddressZero,
+        recipient.address,
+        10
+      )
     ).to.be.revertedWith("ERC20: transfer from the zero address");
     await expect(
-      this.token
-        .connect(this.sender)
-        .transferFrom(this.sender.address, ethers.constants.AddressZero, 10)
+      this.token.transferFrom(sender.address, ethers.constants.AddressZero, 10)
     ).to.be.revertedWith("ERC20: transfer to the zero address");
     await expect(
-      this.token
-        .connect(this.sender)
-        .transferFrom(this.sender.address, this.recipient.address, 15)
+      this.token.transferFrom(sender.address, recipient.address, 15)
     ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
 
-    await this.token.approve(this.sender.address, 10);
+    await this.token.approve(sender.address, 10);
     await expect(
-      this.token
-        .connect(this.sender)
-        .transferFrom(this.sender.address, this.recipient.address, 11)
+      this.token.transferFrom(sender.address, recipient.address, 11)
     ).to.be.revertedWith("ERC20: transfer amount exceeds allowance");
   });
 });
