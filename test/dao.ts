aafba063db30bf02c483cad09c01b0167c52901f
rupikz <kydry.nikolau@gmail.com>
2021-12-19 10:19:50 +0300

    add test for deposit

--- test/dao.ts
+++ test/dao.ts
@@ -1,75 +1,112 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("DAO", function () {
   const decimals = 18;
+  let chairPerson: SignerWithAddress, token: Contract, dao: Contract;
 
   this.beforeEach(async function () {
-    const Contract = await ethers.getContractFactory("DAO");
-    const contract = await Contract.deploy("Nikolai", "NIC", decimals);
-    await contract.deployed();
-    const [sender, recipient, account] = await ethers.getSigners();
-    const zeroSender = await ethers.getSigner(ethers.constants.AddressZero);
-    this.sender = sender;
-    this.recipient = recipient;
-    this.account = account;
-    this.zeroSender = zeroSender;
-    this.contract = contract;
+    [chairPerson] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("Nikolai", "NIC", decimals);
+    await token.deployed();
 
-    await this.contract.mint(this.sender.address, 1000);
+    const DAO = await ethers.getContractFactory("DAO");
+    dao = await DAO.deploy(chairPerson.address, token.address, 10, 259200);
+    await dao.deployed();
+
+    await token.mint(chairPerson.address, 1000);
+  });
+
+  it("Init DAO", async function () {
+    [chairPerson] = await ethers.getSigners();
+    const DAO = await ethers.getContractFactory("DAO");
+
+    // TODO: Как проверить что все хорошо?
+    // await expect(DAO.deploy(chairPerson.address, token.address, 20, 1)).to.be.not;
+
+    await expect(
+      DAO.deploy(chairPerson.address, token.address, 0, 0)
+    ).to.be.revertedWith("DAO: MinimumQuorum should be more: 10");
+
+    await expect(
+      DAO.deploy(chairPerson.address, token.address, 10, 0)
+    ).to.be.revertedWith("DAO: Period duration should be more: 259200");
+
+    await expect(
+      DAO.deploy(ethers.constants.AddressZero, token.address, 10, 259200)
+    ).to.be.revertedWith("DAO: Chairperson from the zero address.");
+
+    await expect(
+      DAO.deploy(chairPerson.address, ethers.constants.AddressZero, 10, 259200)
+    ).to.be.revertedWith("DAO: Token from the zero address.");
   });
 
   it("Deposit", async function () {
-    expect(await this.contract.balanceOf(this.sender.address)).to.equal(1000);
-    expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(0);
-    await this.contract.deposit(100);
-    expect(await this.contract.balanceOf(this.sender.address)).to.equal(900);
-    expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(
-      100
-    );
-    await expect(this.contract.deposit(1000)).to.be.revertedWith(
-      "DAO: transfer amount exceeds balance"
-    );
+    expect(await token.balanceOf(chairPerson.address)).to.equal(1000);
+    expect(await dao.balanceOf(chairPerson.address)).to.equal(0);
+    await token.approve(dao.address, 100);
+    expect(await dao.deposit(100))
+      .to.emit(dao, "Deposit")
+      .withArgs(chairPerson.address, 100);
+    expect(await token.balanceOf(chairPerson.address)).to.equal(900);
+    expect(await dao.balanceOf(chairPerson.address)).to.equal(100);
   });
 
   it("Withdraw", async function () {
-    await expect(this.contract.withdraw(1000)).to.be.revertedWith(
-      "DAO: transfer amount exceeds vote balance"
+    await expect(dao.withdraw(1000)).to.be.revertedWith(
+      "DAO: transfer amount exceeds vote balance."
     );
-    await this.contract.deposit(1000);
-    expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(
-      1000
-    );
-    await this.contract.withdraw(100);
-    expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(
-      900
-    );
+    await token.approve(dao.address, 100);
+    await dao.deposit(100);
+    expect(await dao.balanceOf(chairPerson.address)).to.equal(100);
 
-    // TODO: Добавить тест на активные голосования
-  });
+    // QUESTION: В Чем ошибка?
+    // await token
+    //   .connect(await ethers.getSigner(dao.address))
+    //   .approve(chairPerson.address, 100);
+    // await dao.withdraw(100);
 
-  it("Create Proposal", async function () {
-    // await expect(this.contract.withdraw(1000)).to.be.revertedWith(
-    //   "DAO: transfer amount exceeds vote balance"
+    // expect(await dao.withdraw(100))
+    //   .to.emit(dao, "Withdraw")
+    //   .withArgs(chairPerson.address, 100);
+
+    // TODO: Добавить проверку на активные голосования
+
+    // expect(await dao.balanceOf(chairPerson.address)).to.equal(900);
+    // await dao.withdraw(900);
+    // expect(await dao.balanceOf(chairPerson.address)).to.equal(0);
+    // await expect(dao.withdraw(1)).to.be.revertedWith(
+    //   "DAO: transfer amount exceeds vote balance."
     // );
-    await expect(
-      this.contract.createProposal(ethers.constants.AddressZero, "Test", 10)
-    ).to.be.revertedWith("DAO: transfer from the zero address");
-
-    // string memory _description,
-    // address _recipient,
-    // bytes32 _byteCode,
-    // uint8 _minimumQuorum
-
-    await this.contract.createProposal("Test");
-    expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(
-      1000
-    );
-    await this.contract.withdraw(100);
-    expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(
-      900
-    );
-
-    // TODO: Добавить тест на активные голосования
   });
+
+  // it("Create Proposal", async function () {
+  //   await this.contract.deposit(1000);
+
+  //   // await expect(this.contract.withdraw(1000)).to.be.revertedWith(
+  //   //   "DAO: transfer amount exceeds vote balance"
+  //   // );
+  //   await expect(
+  //     this.contract.createProposal("Test", "Test", ethers.constants.AddressZero, , 10)
+  //   ).to.be.revertedWith("DAO: transfer from the zero address");
+
+  //   // string memory _description,
+  //   // address _recipient,
+  //   // bytes32 _byteCode,
+  //   // uint8 _minimumQuorum
+
+  //   await this.contract.createProposal("Test");
+  //   expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(
+  //     1000
+  //   );
+  //   await this.contract.withdraw(100);
+  //   expect(await this.contract.voteBalanceOf(this.sender.address)).to.equal(
+  //     900
+  //   );
+
+  //   // TODO: Добавить тест на активные голосования
+  // });
 });
